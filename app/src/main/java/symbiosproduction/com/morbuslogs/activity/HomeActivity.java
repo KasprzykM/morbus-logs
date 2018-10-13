5aa6ce426f0837ba9bef1bf7023a014a508a4ddb
MichalKasprzyk <michal0kasprzyk@gmail.com>
2018-10-13 12:37:50 +0200

    + Logout confirmation added.

--- app/src/main/java/symbiosproduction/com/morbuslogs/activity/HomeActivity.java
+++ app/src/main/java/symbiosproduction/com/morbuslogs/activity/HomeActivity.java
@@ -1,10 +1,12 @@
 package symbiosproduction.com.morbuslogs.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -120,21 +122,55 @@ public class HomeActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_logout)
         {
-            AuthUI.getInstance()
-                    .signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Log.d(TAG, "User has been successfully logged out");
-                            Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
-                            startActivity(intent);
-                            HomeActivity.this.finish();
-                        }
-                    });
+            createDialog();
         }
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+    private void createDialog()
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        alertDialogBuilder.setMessage(R.string.message_logout_dialog);
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setTitle(R.string.title_logout_dialog);
+        alertDialogBuilder.setIcon(R.drawable.ic_warning_white_24dp);
+
+        alertDialogBuilder.setPositiveButton(R.string.confirm_logout_dialog,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        handleLogout();
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton(R.string.cancel_logout_dialog,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+
+    private void handleLogout()
+    {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d(TAG, "User has been successfully logged out");
+                        Intent intent = new Intent(HomeActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                        HomeActivity.this.finish();
+                    }
+                });
     }
 }
